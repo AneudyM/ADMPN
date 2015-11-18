32f71b8eda3602aa83c40fca22c8d59b3a7dccc6
AneudyM <aneudy.motacatalino@gmail.com>
2015-11-17 22:07:11 -0500

    Main page

--- web_gui/index.php
+++ web_gui/index.php
@@ -1,1 +1,15 @@
-<?php phpinfo(); ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>ADMPNS</title>
+        <link rel="stylesheet" type="text/css" href="style.css" media="screen">
+    </head>
+    <body>
+        <div id="main">
+            <?php include("include/inc_header.html"); ?>
+            <?php include("include/inc_navmenu.html"); ?>
+            <?php include("include/inc_home.html"); ?>
+        
+        </div>
+    </body>
+</html>
